f420f00590c688caa40edc2b6f42e892c6b627f5
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-01-17 23:10:19 +0700

    Setup migration Environment for User and RoleCode

---
+++ server/src/migrations/create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      banStatus: {
+        type: Sequelize.BOOLEAN
+      },
+      roleID: {
+        type: Sequelize.INTEGER
+      },
+      refundHistoryID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: "TIMESTAMP",
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
