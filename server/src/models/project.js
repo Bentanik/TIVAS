b33eb25bf6b382fd5f1f00307bfb64a4de8cb920
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-01-21 18:12:31 +0700

    Merge code from master

--- server/src/models/project.js
+++ server/src/models/project.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Project.belongsToMany(models.Type)
+      Project.belongsToMany(models.Type, {through:models.TypeOfProject})
       // define association here
     }
   }
--- server/src/models/type.js
+++ server/src/models/type.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Type.belongsToMany(models.Project)
+      Type.belongsToMany(models.Project, {through:models.TypeOfProject})
       // define association here
     }
   }
