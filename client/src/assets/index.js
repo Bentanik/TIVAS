bbb8d97ee70b68c8ed396e811958237c6fcaab7f
Bentanik <kennyjamqw@gmail.com>
2024-01-22 23:18:49 +0700

    Divine footer component

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,8 @@
 const images = {
   iconSearch: require("~/assets/iconSearch.svg").default,
+  twitterFooter: require("~/assets/twitterFooter.svg").default,
+  linkedInFooter: require("~/assets/linkedInFooter.svg").default,
+  facebookFooter: require("~/assets/facebookFooter.svg").default,
 };
 
 export default images;
