70510767ed31992d64386033f7b48ca0ce32b79a
tphatt22@gmail.com <tphatt22@fmail.com>
2024-01-23 07:37:40 +0700

    add hero and blog-owner

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,7 @@
 const images = {
-  iconSearch: require("~/assets/iconSearch.svg").default,
+    iconSearch: require("~/assets/iconSearch.svg").default,
+    heroImg: require("~/assets/heroImg.svg").default,
+    thumbImg: require("~/assets/thumb.svg").default,
 };
 
 export default images;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,23 +1,71 @@
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
 import Navigations from "~/components/Layouts/Navigations";
+import images from "~/assets";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
+const blog_link = {
+    link: "/blog",
+};
 function Home() {
-  return (
-    <div className={cx("home-wrapper")}>
-      {/* Header */}
-      <header className={cx("header")}>
-        {/* Navigations */}
-        <Navigations />
-      </header>
-      {/* Main */}
-      <main>Main</main>
-      {/* Footer */}
-      <div className={cx("footer")}>Footer</div>
-    </div>
-  );
+    return (
+        <div className={cx("home-wrapper")}>
+            {/* Header */}
+            <header className={cx("header")}>
+                {/* Navigations */}
+                <Navigations />
+                {/* Hero */}
+                <div className={cx("hero-wrapper")}>
+                    <img
+                        src={images.heroImg}
+                        alt="Hero Image"
+                        className={cx("hero-img")}
+                    />
+                    <h1 className={cx("hero-title")}>
+                        Welcome to <span>Tivas</span>
+                    </h1>
+                </div>
+            </header>
+            {/* Main */}
+            <main className={cx("content")}>
+                {/* Owner Blog */}
+                <div className={cx("blog-wrapper")}>
+                    <div className={cx("blog-content")}>
+                        {/* Thumb Image */}
+                        <img
+                            src={images.thumbImg}
+                            alt="Thumb Image"
+                            className={cx("thumb-img")}
+                        />
+                        {/*Rigth content*/}
+                        <div className={cx("right-content")}>
+                            <h3 className={cx("right-header")}>
+                                Explore your home Time for payment
+                            </h3>
+                            <p className={cx("desc")}>
+                                We support flexible payments in stages to help
+                                you manage your finances conveniently, while
+                                ensuring your real estate purchase progress goes
+                                smoothly.
+                            </p>
+
+                            {/* <a href="!#"> */}
+                            <Link
+                                to={blog_link.link}
+                                className={cx("blog-link")}
+                            >
+                                Search Property
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            {/* Footer */}
+            <div className={cx("footer")}>Footer</div>
+        </div>
+    );
 }
 
 export default Home;
