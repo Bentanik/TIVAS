52fb2b80534cd694930067367ac0aff85f602345
unknown <doanhvqse170195@fpt.edu.vn>
2024-01-24 19:55:37 +0700

    Payment process

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
 import Navigations from "~/components/Layouts/Navigations";
 import Footer from "~/components/Layouts/Footer";
+import PaymentIntro from "~/components/PaymentIntro/PaymentIntro";
 
 const cx = classNames.bind(styles);
 
@@ -15,6 +16,7 @@ function Home() {
       </header>
       {/* Main */}
       <main>Main</main>
+      <PaymentIntro/>
       {/* Footer */}
       <footer className={cx("footer")}>
         <Footer />
