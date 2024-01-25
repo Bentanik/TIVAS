454e36499de21c2881ad5093b1c39f1a74d1f3d9
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-01-25 17:31:44 +0700

    - Add Cloudinary to upload Image
    - API create project_V1 (Validation not null for inputted, Upload Images to DB as URL from Cloudinary)

---
+++ server/src/services/project.js
@@ -0,0 +1,36 @@
+import db from "../models";
+const cloudinary = require('cloudinary').v2;
+
+export const createNewProject = ({
+    name,
+    description,
+    buildingStatus
+}, fileData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const [project, created] = await db.Project.findOrCreate({
+                where: { name },
+                defaults: {
+                    name,
+                    description,
+                    buildingStatus,
+                    images: fileData?.path,
+                },
+            })
+            resolve({
+                err: created ? 0 : 1,
+                mess: created ? "Create Project Successfully." : "Project Name has been used!",
+            })
+
+            if (fileData && !created) {
+                cloudinary.uploader.destroy(fileData.filename)
+            }
+        } catch (error) {
+            console.log(error);
+            reject(error);
+            if (fileData) {
+                cloudinary.uploader.destroy(fileData.filename)
+            }
+        }
+    })
+}
