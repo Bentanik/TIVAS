969f9905cd8e9709f46178d6a1c82b91f982e592
unknown <doanhvqse170195@fpt.edu.vn>
2024-03-05 00:03:22 +0700

    procejt

--- client/src/configs/routes.js
+++ client/src/configs/routes.js
@@ -1,6 +1,10 @@
 import Home from "~/pages/Home";
 import Destination from "~/pages/Destination";
 import Search from "~/pages/Search";
+import Listing from "~/pages/Listing";
+import ListingDetails from "~/pages/ListingDetails";
+import TimeshareRentals from "~/pages/TimeshareRentals";
+import Policy from "~/pages/Policy";
 const routes = [
   {
     path: "/",
@@ -17,6 +21,26 @@ const routes = [
     component: Search,
     layout: null,
   },
+  {
+    path: "/listing",
+    component: Listing,
+    layout: null,
+  },
+  {
+    path: "/listingdetails",
+    component: ListingDetails,
+    layout: null,
+  },
+  {
+    path: "/timesharerentals",
+    component: TimeshareRentals ,
+    layout: null,
+  },
+  {
+    path: "/policy",
+    component: Policy,
+    layout: null,
+  }
 ];
 
 export default routes;
