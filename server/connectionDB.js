d082a7842d0d3cd78f3fe7a164af58e047691d03
hiennmse170279 <hiennmse170279@fpt.edu.vn>
2024-03-06 17:13:05 +0700

    Upload DB Cloud

--- server/connectionDB.js
+++ server/connectionDB.js
@@ -1,23 +1,44 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
+// const sequelize = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USERNAME_DB,
+//   process.env.PASSWORD_DB,
+//   {
+//     host: process.env.HOST,
+//     dialect: "mysql",
+//     logging: false,
+//   }
+// );
+const sequelizeDB = new Sequelize(
   process.env.DATABASE,
   process.env.USERNAME_DB,
   process.env.PASSWORD_DB,
   {
-    host: process.env.HOST,
+    host: process.env.HOST_CLOUD,
     dialect: "mysql",
     logging: false,
   }
 );
 
-const connection = async () => {
+
+// const connection = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
+const connectionDB = async () => {
   try {
-    await sequelize.authenticate();
+    await sequelizeDB.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
 
-connection();
+//connection();
+
+connectionDB();
