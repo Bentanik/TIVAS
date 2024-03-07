3efe0fe7a316ce9a78e6e5fba743743765c156c8
Bentanik <kennyjamqw@gmail.com>
2024-03-08 06:23:02 +0700

    update

--- client/src/pages/AdminManageProject/index.jsx
+++ client/src/pages/AdminManageProject/index.jsx
@@ -12,50 +12,52 @@ import { useDispatch, useSelector } from "react-redux";
 const cx = classNames.bind(styles);
 
 function AdminManageProject() {
-    const [projectData, setProjectData] = useState([]);
+  const [projectData, setProjectData] = useState([]);
 
-    const dispatch = useDispatch();
-    const currentUser = useSelector((state) => state.auth.login.user);
-    const axiosInstance = createAxios(dispatch, currentUser);
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.auth.login.user);
+  const axiosInstance = createAxios(dispatch, currentUser);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await getAllProject(axiosInstance);
-            console.log(res);
-            if (res?.err === 0) {
-                setProjectData(res.data);
-                console.log(res);
-            }
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getAllProject(axiosInstance);
+      console.log(res);
+      if (res?.err === 0) {
+        setProjectData(res.data);
+        console.log(res);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <div className={cx("wrapper")}>
-            <h1 className={cx("title")}>Manage Project</h1>
-            <div className={cx("content")}>
-                <div className={cx("list-project")}>
-                    <div className={cx("header-list")}>
-                        <div className={cx("header")}>Thumnail</div>
-                        <div className={cx("header")}>Project Name</div>
-                        <div className={cx("header")}>Location Name</div>
-                        <div className={cx("header")}></div>
-                    </div>
-                    {projectData.map((item, index) => {
-                        return (
-                            <ListProject
-                                status={item?.status}
-                                key={index}
-                                price={item?.reservationPrice}
-                                id={item?.id}
-                                image={item?.thumbnailPathUrl}
-                                name={item?.name}
-                                location={item?.location}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
+  return (
+    <div className={cx("wrapper")}>
+      <h1 className={cx("title")}>Manage Project</h1>
+      <div className={cx("content")}>
+        <div className={cx("list-project")}>
+          <div className={cx("header-list")}>
+            <div className={cx("header")}>Thumnail</div>
+            <div className={cx("header")}>Project Name</div>
+            <div className={cx("header")}>Location Name</div>
+            <div className={cx("header")}></div>
+          </div>
+          {projectData.map((item, index) => {
+            return (
+              <ListProject
+                status={item?.status}
+                key={index}
+                price={item?.reservationPrice}
+                id={item?.id}
+                image={item?.thumbnailPathUrl}
+                name={item?.name}
+                location={item?.location}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AdminManageProject;
